ba3e38051e9aac2eaab887e727e71ef4d46fa7ed
mongbien <duyvtph24890@fpt.edu.vn>
2023-09-29 10:11:37 +0700

    Add ChatLieuTest

---
+++ src/main/java/com/example/demo/entity/ChatLieu.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chatlieu")
+@ToString
+public class ChatLieu {
+
+}
