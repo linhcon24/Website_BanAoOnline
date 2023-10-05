a6210ffcf784cc3055ee25e6a284d21dd1b91bff
whatsipmen6303 <xuanhadanba@gmail.com>
2023-10-05 12:27:40 +0700

    show list product

--- src/main/java/com/example/demo/controller/HomePage.java
+++ src/main/java/com/example/demo/controller/HomePage.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.ChiTietSanPham;
+import com.example.demo.service.ChiTietSanPhamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomePage {
+
+    @Autowired
+    private ChiTietSanPhamService chiTietSanPhamService;
+    private Page<ChiTietSanPham> listChiTietSanPham;
 
     @RequestMapping(value = {"", "/", "/index", "/home"})
     public String homePage(Model model) {
@@ -50,7 +61,9 @@ public class HomePage {
     }
 
     @GetMapping("/product")
-    public String product() {
+    public String product(Model model,@RequestParam(defaultValue = "0",name = "num")Integer num) {
+        listChiTietSanPham = chiTietSanPhamService.getAll(num,4);
+        model.addAttribute("list",listChiTietSanPham);
         return "productPage";
     }
 }
