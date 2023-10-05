dee7b931351da487e0989bd30e715e3af918636c
linhcon24 <nguyenkhanhlinhh24@gmail.com>
2023-10-05 12:14:01 +0700

    Update giao dien

---
+++ src/main/java/com/example/demo/bean/LoginForm.java
@@ -0,0 +1,12 @@
+package com.example.demo.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginForm {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/example/demo/bean/RegisterForm.java
@@ -0,0 +1,14 @@
+package com.example.demo.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RegisterForm {
+
+    private String username;
+    private String password;
+    private String rePassword;
+    private String email;
+}
